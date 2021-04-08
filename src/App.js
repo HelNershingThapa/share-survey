e76a886749412d97c5187c2184bd5b8340b91976
Hel Nershing Thapa <helnershingthapa@gmail.com>
2021-04-08 10:22:56 +0545

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import ReactShare from "react-share-simplified";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ReactShare
+        url="https://ee.humanitarianresponse.info/i/a5tPFS2q"
+        title="Survey of Industry's Preparedness for Tourism Recovery"
+        facebook={true} // true or false
+        facebookClass="iconStyle" //className to Style share button
+        googlePlus={true} // true or false
+        googlePlusClass="iconStyle"
+        twitter={true} // true or false
+        twitterClass="iconStyle"
+        email={true} // true or false
+        emailClass="iconStyle"
+        whatsapp={true} // true or false
+        whatsappClass="iconStyle"
+        iconSize={32} // Icon size
+        addClass="styling-className" //add className to style the whole div
+      />
     </div>
   );
 }
